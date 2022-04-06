9318937a934781483b5bb04aaa9a11cd3e2a1c94
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-04-06 10:53:31 -0400

    Add project files.

---
+++ Part 6 - Random Numbers/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Part_6___Random_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.txtMinimum = new System.Windows.Forms.TextBox();
+            this.txtMaximum = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(12, 15);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(105, 13);
+            this.lblMin.TabIndex = 0;
+            this.lblMin.Text = "Enter Minimum Here:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(0, 116);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(242, 91);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(12, 52);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(108, 13);
+            this.lblMax.TabIndex = 2;
+            this.lblMax.Text = "Enter Maximum Here:";
+            // 
+            // txtMinimum
+            // 
+            this.txtMinimum.Location = new System.Drawing.Point(123, 12);
+            this.txtMinimum.Name = "txtMinimum";
+            this.txtMinimum.Size = new System.Drawing.Size(100, 20);
+            this.txtMinimum.TabIndex = 3;
+            // 
+            // txtMaximum
+            // 
+            this.txtMaximum.Location = new System.Drawing.Point(123, 49);
+            this.txtMaximum.Name = "txtMaximum";
+            this.txtMaximum.Size = new System.Drawing.Size(100, 20);
+            this.txtMaximum.TabIndex = 4;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(84, 90);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Generate";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 203);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtMaximum);
+            this.Controls.Add(this.txtMinimum);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblMin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.TextBox txtMinimum;
+        private System.Windows.Forms.TextBox txtMaximum;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ Part 6 - Random Numbers/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_6___Random_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int maxStore;
+        int minStore;
+        Random generator = new Random();
+        int rand1;
+        int rand2;
+        public void RandStore()
+        {
+            int = generator.Next(minStore, maxStore);
+        }
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(txtMaximum.Text, out maxStore))
+            {
+                maxStore = maxStore + 1;
+                if (Int32.TryParse(txtMinimum.Text, out minStore))
+                {
+
+                }
+            }
+        }
+    }
+}
